1a340dc28062c205b38cf39df7366aa966809e01
matthiashermsen <matthiashermsen@gmail.com>
2021-02-28 13:40:01 +0100

    ObserveVoiceChannelCommand improvements

--- DiscordMuteBot/Bot/Commands/ObserveVoiceChannelCommand.cs
+++ DiscordMuteBot/Bot/Commands/ObserveVoiceChannelCommand.cs
@@ -23,28 +23,25 @@ namespace Bot.Commands
         [Summary("Observes a voice channel")]
         public async Task ObserveVoiceChannel([Summary("The id of the voice channel to observe")] ulong voiceChannelId)
         {
-            if (ObservedVoiceChannelsCache.IsObserved(voiceChannelId))
+            IReadOnlyCollection<IVoiceChannel> guildVoiceChannels = await Context.Guild.GetVoiceChannelsAsync();
+            IVoiceChannel voiceChannel = guildVoiceChannels.FirstOrDefault(currentVoiceChannel => currentVoiceChannel.Id == voiceChannelId);
+
+            if (voiceChannel == null)
             {
-                await Context.Channel.SendMessageAsync($"Voice channel with id {voiceChannelId} is already under observation.");
+                await Context.Channel.SendMessageAsync($"Voice channel with id {voiceChannelId} not found on this guild.");
+            }
+            else if (ObservedVoiceChannelsCache.IsObserved(voiceChannelId))
+            {
+                await Context.Channel.SendMessageAsync($"Voice channel {voiceChannel.Name} is already under observation.");
             }
             else
             {
-                IReadOnlyCollection<IVoiceChannel> guildVoiceChannels = await Context.Guild.GetVoiceChannelsAsync();
-                IVoiceChannel voiceChannel = guildVoiceChannels.FirstOrDefault(currentVoiceChannel => currentVoiceChannel.Id == voiceChannelId);
-
-                if (voiceChannel == null)
-                {
-                    await Context.Channel.SendMessageAsync($"Voice channel with id {voiceChannelId} not found on this guild.");
-                }
-                else
-                {
-                    ObservedVoiceChannelsCache.Observe(voiceChannelId);
+                ObservedVoiceChannelsCache.Observe(voiceChannelId);
+                
+                IUserMessage message = await Context.Channel.SendMessageAsync($"Observing voice channel {voiceChannel.Name}.");
                     
-                    IUserMessage message = await Context.Channel.SendMessageAsync($"Observing voice channel with id {voiceChannelId}.");
-                    
-                    IEmote emote = new Emoji(_observedVoiceChannelOptionsMonitor.CurrentValue.MutedEmoji);
-                    await message.AddReactionAsync(emote);
-                }
+                IEmote emote = new Emoji(_observedVoiceChannelOptionsMonitor.CurrentValue.MutedEmoji);
+                await message.AddReactionAsync(emote);
             }
         }
     }
