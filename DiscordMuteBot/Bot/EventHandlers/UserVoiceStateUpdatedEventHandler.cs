24c1fcdf97f848964537ab9ad741f68e9ac71054
matthiashermsen <matthiashermsen@gmail.com>
2021-03-13 10:42:47 +0100

    UserVoiceStateUpdatedEventHandler

--- DiscordMuteBot/Bot/EventHandlers/UserVoiceStateUpdatedEventHandler.cs
+++ DiscordMuteBot/Bot/EventHandlers/UserVoiceStateUpdatedEventHandler.cs
@@ -1,10 +1,18 @@
 using System.Threading.Tasks;
+using Discord;
 using Discord.WebSocket;
 
 namespace Bot.EventHandlers
 {
     public sealed class UserVoiceStateUpdatedEventHandler
     {
+        private readonly ObservedVoiceChannelsCache _observedVoiceChannelsCache;
+        
+        public UserVoiceStateUpdatedEventHandler(ObservedVoiceChannelsCache observedVoiceChannelsCache)
+        {
+            _observedVoiceChannelsCache = observedVoiceChannelsCache;
+        }
+        
         public async Task OnUserVoiceStateUpdated(
             SocketUser socketUser, 
             SocketVoiceState oldSocketVoiceState,
@@ -12,14 +20,76 @@ namespace Bot.EventHandlers
         {
             if (socketUser is SocketGuildUser socketGuildUser)
             {
-                /*
-                    TODO
+                bool userIsMuted = socketGuildUser.VoiceState?.IsMuted == true;
+                bool userIsNotOffline = socketGuildUser.Status != UserStatus.Offline;
+                
+                // user left observed muted voice channel
+                if (oldSocketVoiceState.VoiceChannel != null && 
+                    newSocketVoiceState.VoiceChannel == null &&
+                    _observedVoiceChannelsCache.TryGetValue(oldSocketVoiceState.VoiceChannel.Id, out ObservedVoiceChannel observedLeftVoiceChannel) &&
+                    observedLeftVoiceChannel.IsMuted &&
+                    userIsMuted &&
+                    userIsNotOffline
+                    )
+                {
+                    await socketGuildUser.ModifyAsync(guildUserProperties => guildUserProperties.Mute = false);
+                }
+                // user joined observed muted voice channel
+                else if (oldSocketVoiceState.VoiceChannel == null && 
+                         newSocketVoiceState.VoiceChannel != null &&
+                         _observedVoiceChannelsCache.TryGetValue(newSocketVoiceState.VoiceChannel.Id, out ObservedVoiceChannel observedJoinedVoiceChannel) &&
+                         observedJoinedVoiceChannel.IsMuted &&
+                         !userIsMuted &&
+                         userIsNotOffline)
+                {
+                    await socketGuildUser.ModifyAsync(guildUserProperties => guildUserProperties.Mute = true);
+                }
+                // user changed voice channels
+                else if (oldSocketVoiceState.VoiceChannel != null && 
+                         newSocketVoiceState.VoiceChannel != null &&
+                         userIsNotOffline)
+                {
+                    bool oldVoiceChannelObserved = _observedVoiceChannelsCache.TryGetValue(
+                        oldSocketVoiceState.VoiceChannel.Id, out ObservedVoiceChannel oldObservedVoiceChannel);
                     
-                    If observed voice channel is muted
-                    
-                        mute if a new user joins
-                        unmute if a user leaves
-                */
+                    bool newVoiceChannelObserved = _observedVoiceChannelsCache.TryGetValue(
+                        newSocketVoiceState.VoiceChannel.Id, out ObservedVoiceChannel newObservedVoiceChannel);
+
+                    // user moved from observed muted voice channel to unobserved voice channel
+                    if (oldVoiceChannelObserved && 
+                        !newVoiceChannelObserved &&
+                        oldObservedVoiceChannel.IsMuted &&
+                        userIsMuted)
+                    {
+                        await socketGuildUser.ModifyAsync(guildUserProperties => guildUserProperties.Mute = false);
+                    }
+                    // user moved from unobserved voice channel to observed muted voice channel
+                    else if (!oldVoiceChannelObserved && 
+                             newVoiceChannelObserved &&
+                             newObservedVoiceChannel.IsMuted &&
+                             !userIsMuted)
+                    {
+                        await socketGuildUser.ModifyAsync(guildUserProperties => guildUserProperties.Mute = true);
+                    }
+                    // both voice channels are observed
+                    else if (oldVoiceChannelObserved && newVoiceChannelObserved)
+                    {
+                        // user moved from muted to unmuted voice channel
+                        if (oldObservedVoiceChannel.IsMuted && 
+                            !newObservedVoiceChannel.IsMuted &&
+                            userIsMuted)
+                        {
+                            await socketGuildUser.ModifyAsync(guildUserProperties => guildUserProperties.Mute = false);
+                        }
+                        // user moved from unmuted to muted voice channel
+                        else if (!oldObservedVoiceChannel.IsMuted && 
+                                 newObservedVoiceChannel.IsMuted && 
+                                 !userIsMuted)
+                        {
+                            await socketGuildUser.ModifyAsync(guildUserProperties => guildUserProperties.Mute = true);
+                        }
+                    }
+                }
             }
         }
     }
